0d7561736c0d75136ea2d57e84bec5d42e3b39fc
Eugene M <rusglobal@gmail.com>
2024-02-04 21:53:33 +0300

    add lesson3

---
+++ LESSON3/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿Random rnd = new Random();
+int size = 500;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    arr_int[i] = rnd.Next(1, 10);
+    i = i + 1;
+}
+i = 0;
+while (i < size)
+{
+    Console.Write($"{arr_int[i]} ");
+    i = i + 1;
+}
+
+size = 10;
+int[] arr_int2 = new int[size];
+i = 0;
+while (i < size)
+{
+    System.Console.WriteLine($"Введите элемент массива с индексом {i}:");
+    string input = Console.ReadLine();
+    arr_int2[i] = Convert.ToInt32(input);
+    i = i + 1;
+}
+i = 0;
+while (i < size)
+{
+    Console.Write($"{arr_int2[i]} ");
+    i = i + 1;
+}
