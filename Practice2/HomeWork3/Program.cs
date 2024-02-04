ea742d9b4b692cefdde0d92a051b48e7cd69f16a
Eugene M <rusglobal@gmail.com>
2024-02-04 15:49:04 +0300

    add Homework2

---
+++ Practice2/HomeWork3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int number = 3;
+if (number >= 10 && number <= 99)
+{
+    int maxDigit = 0;
+
+    while (number > 0)
+    {
+        int digit = number % 10;
+        maxDigit = Math.Max(maxDigit, digit);
+        number /= 10;
+    }
+
+    Console.WriteLine($"Наибольшая цифра в числе - {maxDigit}.");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод. Введите целое число от 10 до 99.");
+}
---
+++ Practice2/HomeWork4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.Write($"Цифры числа через запятую: ");
+int n = 597423;
+int divisor = 1;
+bool isFirstDigit = true;
+
+while (n / divisor >= 10)
+{
+    divisor *= 10;
+}
+while (divisor > 0)
+{
+    int digit = n / divisor;
+    n %= divisor;
+    divisor /= 10;
+
+    if (!isFirstDigit)
+    {
+        Console.Write(", ");
+    }
+
+    Console.Write(digit);
+    isFirstDigit = false;
+}
+
+Console.WriteLine();
+
