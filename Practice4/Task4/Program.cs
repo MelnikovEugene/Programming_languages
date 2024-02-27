d7d437eb72700ec7d90468dab903e7058da06a3d
Eugene M <rusglobal@gmail.com>
2024-02-27 19:47:12 +0300

    add Task4

---
+++ Practice4/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿int [] CreateRandomArray(int min, int max, int size){
+    int [] array = new int [size];
+    for(int i=0; i<size; i++){
+        array[i] = new Random().Next(min,max+1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array){
+    foreach(int item in array){
+        Console.Write(item+" ");
+    }
+}
+
+int CountOfOdd(int [] array){
+    int count = 0;
+    foreach(int item in array){
+        if(item%2 != 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Input min value:");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max value:");
+int maximum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input size value:");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+int [] array = CreateRandomArray(minimum,maximum,lenght);
+ShowArray(array);
+int count = CountOfOdd(array);
+Console.WriteLine($"\nCount of odd numbers in array is {count}");
