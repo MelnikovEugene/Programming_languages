b0588008948c9705f91d070cb300e17e1f28be30
Eugene M <rusglobal@gmail.com>
2024-03-14 17:58:21 +0300

    add Homeworks 1,2,3 to Practice7

---
+++ Practice7/Homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int M = 5;
+int N = 15;
+void PrintNaturalNumbers(int current, int end)
+{
+    if (current > end)
+    {
+        return;
+    }
+    Console.Write($"{current} ");
+    PrintNaturalNumbers(current + 1, end);
+}
+PrintNaturalNumbers(M, N);
---
+++ Practice7/Homework3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+
+void PrintArrayReverse(int[] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+    Console.Write($"{array[index]} ");
+    PrintArrayReverse(array, index - 1);
+}
+
+PrintArrayReverse(array, array.Length - 1);
